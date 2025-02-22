0f1069741ec0e702c1330a5ccb46c385b4d91523
mopper97 <mattia.opper@gmail.com>
2025-02-22 20:55:43 +0100

    dgl version working torch native bugged

--- src/model_dgl.py
+++ src/model_dgl.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_sequence
 import numpy as np
 from tqdm import tqdm
+import dgl
+import dgl.function as fnc
 
 from utils import create_dataloader
 
@@ -58,6 +60,28 @@ def get_complete(frontiers):
     return ~torch.all(frontiers[:, 1:] == -1, dim=1)
 
 @torch.no_grad()
+def get_neighbours(index, retrieval, range_tensor):
+    n_hood = torch.zeros(range_tensor.shape[0], 3, device=range_tensor.device)
+
+    # step 1: gets left the composed indices 
+    targs = index[range_tensor, retrieval].view(-1, 2)
+    # now we need the left of the lefts 
+    lefts = targs[:, 0] 
+    l_s = index[range_tensor[::2], retrieval[::2]-1] # set to -1 this will retrieve <sos> from the node stack later on 
+    l_s[retrieval[::2]-1 == -1] = -1 # protect against leak for the longest sequence
+    rights = targs[:, 1]
+    l_t = torch.cat((lefts.unsqueeze(1), l_s.unsqueeze(1), rights.unsqueeze(1)), dim=-1)
+    # set the lefts
+    n_hood[:l_t.shape[0]] = l_t
+    
+    r_s = index[range_tensor[::2], torch.clamp(retrieval[1::2]+1, max=index.shape[1]-1)] # protect against leak for the longest sequence
+    r_s[retrieval[1::2]+1 == index.shape[1]] = -2 # set to -2 this will retrieve <eos> from the node stack later on
+    r_t = torch.cat((rights.unsqueeze(1), lefts.unsqueeze(1), r_s.unsqueeze(1)), dim=-1)
+    # set the rights
+    n_hood[l_t.shape[0]:] = r_t
+    return n_hood
+
+@torch.no_grad()
 def get_sims(nodes, index):
     # empty to tensor to perform the similarity check
     sims = torch.full((index.shape[0], index.shape[1], nodes.shape[1]), -np.inf, device=nodes.device)
@@ -93,102 +117,104 @@ class Banyan(nn.Module):
         self.E = e_dim 
         self.V = vocab_size
         self.embedding = nn.Embedding(self.V, self.E)
-        self.comp_fn = LCCompose(self.E, 4)
-        # node cache: to hold node features 
-        self.register_buffer('n_c', torch.zeros(max_nodes, self.E, device=device, requires_grad=False))
-        # target cache: to hold the target values for the nodes
-        self.register_buffer('t_c', torch.full((max_nodes, 3), -1, device=device, requires_grad=False)) # [target, left, right] 
-        # composition cache: to hold the children of the nodes
-        self.register_buffer('c_c', torch.full((max_nodes, 2), -1, device=device, requires_grad=False)) # [left, right]
-        # just some other junk
+        self.comp_fn = LCCompose(self.E, 16)
+        # <sos> and <eos> tokens    
+        self.sos = nn.Parameter(torch.zeros(self.E), requires_grad=True)
+        self.eos = nn.Parameter(torch.zeros(self.E), requires_grad=True)
         self.dropout = nn.Dropout(0.1)
         self.device = device
 
-    @torch.no_grad()
-    def reset_cache(self):
-        # self.n_c.fill_(0)
-        # self.t_c.fill_(-1)
-        # self.c_c.fill_(-1)
-        # Instead of in-place fill_
-        self.n_c = torch.zeros_like(self.n_c)
-        self.t_c = torch.full_like(self.t_c, -1)
-        self.c_c = torch.full_like(self.c_c, -1)
 
-    def update_graph(self, retrieval, index, md):
+
+    def update_graph(self, graph, retrieval, index):
         # range used for indexing
         range_tensor = torch.arange(index.shape[0], device=index.device, dtype=torch.long).repeat_interleave(2)
         # get src
         src = index[range_tensor, retrieval].view(-1,2)
-        # check whether parents already cached and filter
-        mask = ~torch.eq(src.unsqueeze(1), self.c_c).all(dim=2).any(dim=1) # this is weird check sober
-        src = src[mask]
-        # make sure src is unique (no duplicate parents)
-        src = torch.unique(src, dim=0) 
-        if not src.numel() == 0:
-            # set indices for the new nodes 
-            dst = md + 1 + torch.arange(src.shape[0], device=src.device)
-            md = torch.max(dst)
-            self.n_c[dst] = self.comp_fn(self.n_c[src].view(-1, 2, 4, 4))
-            # add composition indices to the cache
-            self.c_c[dst] = src
-            # add target indices to the cache
-            l_t  = torch.cat((retrieval[::2], (retrieval[::2]-1), retrieval[1::2]), dim=0)
-            # make sure right neighbour is not out of bounds
-            r_n = retrieval[1::2] + 1
-            r_n[r_n > torch.max(index)] = -1  
-            r_t = torch.cat((retrieval[1::2], retrieval[::2], r_n), dim=0)
-            # apply same filtering as to src to ensure no duplicates
-            t = torch.cat((l_t, r_t), dim=0).view(-1, 3)
-            t = torch.unique(t, dim=0)
-            mask = ~torch.eq(t.unsqueeze(1), self.t_c).all(dim=2).any(dim=1)
-            t = t[mask]
-            # add target indices to the cache
-            adds = (self.t_c != -1).all(dim=1).sum()
-            self.t_c[adds: adds + t.shape[0]] = t
-        return index, md
+        # get existing edges in the graph
+        ex_src, ex_dst = graph.edges() if graph.num_edges() > 0 else (None, None)
+        # get the new src and dst 
+        if ex_src is not None:
+            # check whether edges already exist in graph
+            mask = ~torch.eq(src.unsqueeze(1), ex_src.view(-1, 2)).all(dim=2).any(dim=1)
+            # filter src according to mask
+            src = src[mask]
+            # make sure the new edges are unique
+            src = torch.unique(src, dim=0) 
+            # set indices for the new nodes
+            dst = torch.max(ex_dst) + 1 + torch.arange(src.shape[0], device=src.device)
+        else:
+            src = torch.unique(src, dim=0)
+            dst = torch.max(index) + 1 + torch.arange(src.shape[0], device=src.device)
+        # update the graph
+        # 1. add the new nodes and their representations 
+        graph.add_nodes(dst.shape[0], {'comp': self.comp_fn(graph.ndata['comp'][src].view(-1, 2, 16, 16))})
+        # 2. add the new edges
+        graph.add_edges(src.flatten(), dst.repeat_interleave(2).flatten())
+        # save neighbours 
+        # print('ind n block')
+        # print(index)
+        n_hood = get_neighbours(index, retrieval, range_tensor)
+        # print(n_hood)
+        # print('\n')
+        # update index tensor 
+        # 1. recreate the original src tensor
+        src = index[range_tensor, retrieval].view(-1,2)
+        # 2. find which edges contain src
+        ex_src, ex_dst = graph.edges()
+        locs = torch.where(src.unsqueeze(1) == ex_src.view(-1, 2), 1, 0).all(dim=-1).nonzero()[:, 1]
+        # 3. get the corresponding values from dst and fill index accordingly 
+        update = ex_dst.view(-1, 2)[locs] 
+        index[range_tensor, retrieval] = update.view(-1)
+        # return the updated graph and index tensor
+        return graph, index, n_hood
 
 
-    def compose(self, seqs):
+
+    def compose(self, seqs, roots=False):
         # we need the range for several operations and no point casting it to cuda each time
         range_tensor = torch.tensor(range(seqs.shape[0]), dtype=torch.long, device=self.device)
-        # index - tensor that is going to keep track of which nodes constitute the frontier
-        # leaves - indices to get the initial embeddings from the embedding matrix
-        index, leaves = create_index(seqs)
-        # add the initial leaves to the node cache
-        self.n_c[:leaves.shape[0]] = self.dropout(self.embedding(leaves))
-        # get the max of index as the initial max dist
-        md = torch.max(index)
+        # embed the initial froniter 
+        frontier = self.embedding(seqs) # shape (b_size, seq_len, E)
+        # get the node indexes for the embeddings and the tokens they map to 
+        index, tokens = create_index(seqs) # index shape (b_size, seq_len) tokens shape set(tokens in seqs)
+        # create the graph 
+        g = dgl.graph(([], []), device=self.device) # empty graph
+        g.add_nodes(tokens.shape[0]) # add the nodes
+        g.ndata['comp'] = self.dropout(self.embedding(tokens)) # add the embeddings for the nodes
+        targets = []
         # reduce the frontiers till we get to the root 
         while index.shape[1] != 1:
             # get the merge indices
-            max_sim, retrieval = get_sims(self.n_c.detach(), index)
+            max_sim, retrieval = get_sims(g.ndata['comp'].detach(), index)
             # get the completion mask
             completion_mask = get_complete(index)
             # update the graph 
-            # check corner case where max index is masked out by completion mask and therefore you might be overwriting rows in the cache
-            index[completion_mask], md = self.update_graph(retrieval[completion_mask.repeat_interleave(2)], index[completion_mask], md)
+            g, index[completion_mask], n_hood = self.update_graph(g, retrieval[completion_mask.repeat_interleave(2)], index[completion_mask])
+            # add neighbours to targets
+            targets.append(n_hood)
             # reduce the frontiers
             index = reduce_frontier(index, completion_mask, range_tensor, max_sim)
+        # cat the targets
+        targets = torch.cat(targets, dim=0)
+        # add the <sos> and <eos> tokens to the graph
+        g.add_nodes(2, {'comp': torch.stack([self.eos, self.sos])})
+        return g, targets.long()
 
-        
-        # return all filled values of node cache 
-        nodes = self.n_c[:md + 1]
-        # return all filled values of target cache
-        targets = self.t_c[(self.t_c != -1).all(dim=1)]
-        return nodes, targets
 
 
 
     def forward(self, x):
         # reset cache 
-        self.reset_cache()
-        nodes, targets = self.compose(x)
+        g, targets = self.compose(x)
+        nodes = g.ndata['comp']
+        # print('final')
+        # print(targets)
         lefts = nodes[targets[:, 1]]
         rights = nodes[targets[:, 2]]
-        outs = self.comp_fn(torch.cat((lefts, rights), dim=1).view(-1, 2, 4,4))
+        outs = self.comp_fn(torch.cat((lefts, rights), dim=1).view(-1, 2, 16,16))
         outs = outs @ nodes.T
         return F.cross_entropy(outs, targets[:, 0])
-            
 
 
 
@@ -196,14 +222,15 @@ class Banyan(nn.Module):
 
 
 
-dl = create_dataloader('/Users/mopper/Desktop/All-The-Way-Up/data/small_train.txt', 2, shuffle=True)
-model = Banyan(16, 25001, 'cpu')
+dl = create_dataloader('/Users/mopper/Desktop/All-The-Way-Up/data/small_train.txt', 256, shuffle=True)
+model = Banyan(256, 25001, 'cpu')
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 model.train()
 # with torch.autograd.set_detect_anomaly(True):
 for e in range(10):
     epoch_loss = 0
     for i in tqdm(dl):
+        # model(i)
         optimizer.zero_grad()
         loss = model(i)
         loss.backward()
